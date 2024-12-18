21492711b467a799a4dd142b44b7a9af814243d4
Mayaz120102 <abrarmayaz2002@gmail.com>
2024-12-19 01:49:22 +0600

    user_views.py

--- app_backend/api/views/user_views.py
+++ app_backend/api/views/user_views.py
@@ -15,7 +15,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from api.models import *
-from api.serializers import UserSerializer
+from api.serializers import UserSerializer,UserSerializerWithToken
 
 #jwt views
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -42,3 +42,64 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes =[
+        '/api/products/',
+        '/api/products/<id>',
+        '/api/users',
+        '/api/users/register',
+        '/api/users/login',
+        '/api/users/profile',
+    ]
+    return Response(routes)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            password = make_password(data['password']),
+        )
+
+        serializer = UserSerializerWithToken(user,many=False)
+        return Response(serializer.data)
+    
+    except:
+        message = {"detail":"User with this email is already registered"}
+        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user =request.user 
+    serializer = UserSerializer(user,many = False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user =request.user 
+    serializer = UserSerializerWithToken(user,many = False)
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    if data['password'] !="":
+        user.password= make_password(data['password'])
+    user.save()
+    return Response(serializer.data)
+
+
+
+@api_view(['DELETE'])
+def deleteUser(request,pk):
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion.delete()
+    return Response("User was deleted")
