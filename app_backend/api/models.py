2cb30e53ae20da95bc341629a40ab0648b81a5c3
ahammedshorf <ahammedshorif20@gmail.com>
2024-12-19 16:54:40 +0600

    models done

--- app_backend/api/models.py
+++ app_backend/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import BLANK_CHOICE_DASH
 
+# product models
 class Product(models.Model):
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200,null=True,blank=True)
@@ -21,7 +22,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name +" | "+self.brand +" | " + str(self.price)
     
-
+#review models
 class Review(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
@@ -33,5 +34,50 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.rating)
+    
+# order models
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    paymentMethod = models.CharField(max_length=200,null=True,blank=True)
+    taxPrice = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    shippingPrice = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    totalPrice = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False,null=True, blank=True)
+    isDeliver = models.BooleanField(default=False)
+    deliveredAt = models.DateTimeField(auto_now_add=False,null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True,null=True, blank=True)
+    _id =  models.AutoField(primary_key=True,editable=False)
+
+    def __str__(self):
+        return str(self.createdAt)    
+
+#order Item models    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
+    order  = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+    name = models.CharField(max_length=200,null=True,blank=True)
+    qty = models.IntegerField(null=True,blank=True,default=0)
+    price = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    image = models.CharField(max_length=200,null=True,blank=True)
+    _id =  models.AutoField(primary_key=True,editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
+# shipping address models
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order,on_delete=models.CASCADE,null=True,blank=True)
+    address = models.CharField(max_length=200,null=True,blank=True)
+    city  = models.CharField(max_length=200,null=True,blank=True)
+    postalCode = models.CharField(max_length=200,null=True,blank=True)
+    country = models.CharField(max_length=200,null=True,blank=True)
+    shippingPrice = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+    _id = models.AutoField(primary_key=True,editable=False)
+
+    def __str__(self):
+        return str(self.address)
         
     
